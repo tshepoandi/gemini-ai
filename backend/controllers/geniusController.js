a1e4d1f745b8a6bd4a106968832511b501459632
tshepoandi <lovekilledtshepo@gmail.com>
2024-05-24 10:12:05 +0200

    backend to front setup on genius

--- backend/controllers/geniusController.js
+++ backend/controllers/geniusController.js
@@ -3,14 +3,20 @@ import genius from 'genius-lyrics'
 const Client = new genius.Client(process.env.GENUIS_API_KEY)
 
 const searchController = async(req, res) => {
-    const searches = await Client.songs.search("faded");
-
-    // Pick first one
-    const everysearch = searches;
-    console.log("About the Song:\n", everysearch, "\n");
-    res.status(200).json({
-        search: everysearch
-    })
+    try {
+        const searches = await Client.songs.search("if looks could kill");
+        const responseData = searches.map(song => ({
+            id: song.id,
+            title: song.title,
+            artist: song.artist.name,
+            url: song.url,
+            thumbnail: song.thumbnail
+        }));
+        res.status(200).json(responseData);
+    } catch (error) {
+        console.error("Error occurred:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 
 export default searchController
